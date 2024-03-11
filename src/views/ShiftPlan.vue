70c50caf93e8d601d697b1a8474b1483cfded5a4
Insa <insa.sangs@rewe-group.com>
2024-03-11 06:34:29 +0100

    start to add veevalidate

--- src/views/ShiftPlan.vue
+++ src/views/ShiftPlan.vue
@@ -1,48 +1,35 @@
 <template>
-  <div>
-    <v-sheet
-      class="d-flex"
-      height="54"
-      tile
-    >
-      <v-select
-        v-model="type"
-        :items="types"
-        class="ma-2"
-        label="View Mode"
-        variant="outlined"
-        dense
-        hide-details
-      ></v-select>
-      <v-select
-        v-model="weekday"
-        :items="weekdays"
-        class="ma-2"
-        label="weekdays"
-        variant="outlined"
-        dense
-        hide-details
-      ></v-select>
-    </v-sheet>
-    <v-sheet>
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :events="events"
-        :view-mode="type"
-        :weekdays="weekday"
-      ></v-calendar>
-    </v-sheet>
-  </div>
+  <v-row>
+    <v-col cols="12">
+      <v-sheet>
+        <v-select
+          v-model="weekday"
+          :items="weekdays"
+          class="ma-2"
+          label="weekdays"
+          variant="outlined"
+          dense
+          hide-details
+        ></v-select>
+      </v-sheet>
+    </v-col>
+    <v-col cols="12">
+      <v-sheet>
+        <v-calendar
+          ref="calendar"
+          v-model="today"
+          :events="events"
+          color="primary"
+          type="week"
+        ></v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
-
 <script>
-  import { useDate } from 'vuetify'
-
   export default {
     data: () => ({
-      type: 'month',
-      types: ['month', 'week', 'day'],
+      today: '2019-01-08',
       weekday: [0, 1, 2, 3, 4, 5, 6],
       weekdays: [
         { title: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
@@ -50,47 +37,49 @@
         { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
         { title: 'Mon, Wed, Fri', value: [1, 3, 5] },
       ],
-      value: [new Date()],
-      events: [],
-      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-      titles: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+      events: [
+        {
+          title: 'Weekly Meeting',
+          start: new Date('2019-01-07 09:00'),
+          end: new Date('2019-01-07 10:00'),
+        },
+        {
+          title: `Thomas' Birthday`,
+          start: new Date('2019-01-10'),
+          end: new Date('2019-01-10'),
+          allDay: true,
+        },
+        {
+          title: 'Mash Potatoes',
+          start: new Date('2019-01-09 12:30'),
+          end: new Date('2019-01-09 15:30'),
+        },
+      ],
     }),
-    mounted () {
-      const adapter = useDate()
-      this.getEvents({ start: adapter.startOfDay(adapter.startOfMonth(new Date())), end: adapter.endOfDay(adapter.endOfMonth(new Date())) })
-    },
-    methods: {
-      getEvents ({ start, end }) {
-        const events = []
-        const min = start
-        const max = end
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days + 20)
-
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
-
-          events.push({
-            title: this.titles[this.rnd(0, this.titles.length - 1)],
-            start: first,
-            end: second,
-            color: this.colors[this.rnd(0, this.colors.length - 1)],
-            allDay: !allDay,
-          })
-        }
-
-        this.events = events
-      },
-      getEventColor (event) {
-        return event.color
-      },
-      rnd (a, b) {
-        return Math.floor((b - a + 1) * Math.random()) + a
-      },
-    },
   }
 </script>
+<style scoped>
+.my-event {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #3b6ea8;
+  color: #ffffff;
+  border: 1px solid #3b6ea8;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+  left: 4px;
+  margin-right: 8px;
+  position: relative;
+}
+
+.my-event.with-time {
+  position: absolute;
+  right: 4px;
+  margin-right: 0px;
+}
+</style>
+
