67e7a1c052f444e029ce5941217844a2b5dde972
Insa <insa.sangs@rewe-group.com>
2024-03-16 14:53:22 +0100

    events are 8 hours long

--- frontend/src/views/ShiftPlan.vue
+++ frontend/src/views/ShiftPlan.vue
@@ -39,6 +39,14 @@
             {{ $refs.calendar.title }}
           </v-toolbar-title>
           <v-spacer></v-spacer>
+          <v-btn            
+            outlined
+            class="mr-4"
+            color="grey darken-2"
+            @click="addShift"
+            >Add Shift
+          </v-btn>
+          <v-spacer></v-spacer>
         </v-toolbar>
       </v-sheet>
       <v-sheet height="600">
@@ -64,7 +72,7 @@
       focus: '',
       events: [],
       colors: ['blue', 'indigo', 'deep-purple'],
-      names: ['Early Shift', 'Middle Shift', 'Late Shift'],
+      names: ['Early Shift', 'Middle Shift', 'Late Shift', 'Night Shift'],
       categories: ['Insa Sangs', 'Malte Sangs'],
     }),
     mounted () {
@@ -85,6 +93,8 @@
       },
       fetchEvents ({ start, end }) {
         const events = []
+        const usedDateByCategory = new Map()
+
 
         const min = new Date(`${start.date}T00:00:00`)
         const max = new Date(`${end.date}T23:59:59`)
@@ -92,20 +102,26 @@
         const eventCount = this.rnd(days, days + 20)
 
         for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
+          const allDay = this.rnd(0, 1) === 0
           const firstTimestamp = this.rnd(min.getTime(), max.getTime())
           const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
+          const secondTimestamp = 8
+          const second = new Date(first.getTime() + secondTimestamp * 3600000)
 
-          events.push({
-            name: this.names[this.rnd(0, this.names.length - 1)],
-            start: first,
-            end: second,
-            color: this.colors[this.rnd(0, this.colors.length - 1)],
-            timed: !allDay,
-            category: this.categories[this.rnd(0, this.categories.length - 1)],
-          })
+          const category = this.categories[this.rnd(0, this.categories.length -1)]
+          const currentDateByCategory = usedDateByCategory.get(category)
+
+          if (!currentDateByCategory || currentDateByCategory.getTime() !== first.getTime()) {
+      events.push({
+              name: this.names[this.rnd(0, this.names.length - 1)],
+              start: first,
+              end: second,
+              color: this.colors[this.rnd(0, this.colors.length - 1)],
+              timed: !allDay,
+              category,
+            })
+            usedDateByCategory.set(category, new Date(first.getTime()))
+          }
         }
 
         this.events = events
